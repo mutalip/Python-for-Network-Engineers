6ad2ee2e94fc375fea5d480aced27772e18e2dfe
Mutalip Kurban <mutalip.kurban@gmail.com>
2017-12-30 14:34:34 -0800

    Create devices.py

---
+++ Netmiko/devices.py
@@ -0,0 +1,23 @@
+  GNU nano 2.7.4                                                 File: devices1.py                                                            
+
+#!/usr/bin/env python3
+s1 = {
+        'device_type': 'cisco_ios',
+        'ip': 'xxxx',
+        'username': 'xxx',
+        'password': 'xxxxxx',
+}
+
+s2 = {
+        'device_type': 'cisco_ios',
+        'ip': 'xxxx',
+        'username': 'xxx',
+        'password': 'xxxxxx',
+}
+
+s3 = {
+        'device_type': 'cisco_ios',
+        'ip': 'xxxx',
+        'username': 'xxx',
+        'password': 'xxxxxx',
+}

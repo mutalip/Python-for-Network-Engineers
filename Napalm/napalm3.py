654a634953ce7f389e709d34c03d4f9a5cb22e37
Mutalip Kurban <mutalip.kurban@gmail.com>
2018-01-02 11:22:20 -0800

    Update napalm3.py

--- Napalm/napalm3.py
+++ Napalm/napalm3.py
@@ -8,7 +8,7 @@ iosvl2 = driver('192.168.122.72', 'david', 'cisco')
 iosvl2.open()
  
 ios_output = iosvl2.get_facts()
-print (json.dumps(bgp_neighbors, indent=4))
+print (json.dumps(ios_output, indent=4))
  
 bgp_neighbors = iosvl2.get_bgp_neighbors()
 print (json.dumps(bgp_neighbors, indent=4))

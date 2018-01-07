5bc24a4ae0434f464a937c41531027a81a626515
Mutalip Kurban <mutalip.kurban@gmail.com>
2018-01-07 11:10:15 -0800

    Update napalm11.py

--- Napalm/napalm11.py
+++ Napalm/napalm11.py
@@ -6,7 +6,7 @@ import jason
 
 from napalm import get_network_driver
 driver = get_network_driver('ios')
-iosvl2 = driver('192.168.3.5','admin','B.h.d.c.switcH')
+iosvl2 = driver('192.168.x.x','admin','***********')
 iosvl2.open()
 ios_output =  iosvl2.get_facts()
 #print (ios_output)
